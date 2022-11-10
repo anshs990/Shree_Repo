04524a2469163ceaa555a76626c14a6890ddc9ed
Shree Ram <anshs990@gmail.com>
2022-11-10 11:45:19 +0530

    step 2

---
+++ CN/h.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"enter number";
+    cin>>n;
+    cout<<"Hello World"<<endl;
+    for (int i =0; i < 5; i++){
+
+        cout<<i<<endl;
+    }
+}
+
+
+
+
+
